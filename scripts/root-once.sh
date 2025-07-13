b7115b4ed67684a849a79a4d01533820e3613136
noranuko13 <noranuko13@gmail.com>
2025-07-14 07:18:42 +0900

    feat: [Docker] Provisioning
    # Conflicts:
    #	Vagrantfile

--- scripts/root-once.sh
+++ scripts/root-once.sh
@@ -3,3 +3,18 @@
 source "/vagrant/scripts/utils/methods.sh"
 
 info "User: $USER, Run: Once"
+
+# Docker
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker vagrant
+info "$(docker --version)"
